2fb5dc5d0ee497d9757467e3e8f4f2fbaafa0449
Robson Cruz <rcflorestal@yahoo.com.br>
2023-05-06 17:17:47 -0300

    Funcao para calcular a mediana de um vetor

---
+++ Estatistica/median_function.R
@@ -0,0 +1,26 @@
+#' Calcula a mediana de um vetor de dados
+#' 
+#' @param x vetor
+#' @return mediana de um vetor
+#' @author Robson Cruz
+#' @examples \dontrun{
+#' a <- c(10, 3, 4, 8, 9, 2)    # 6
+#' b <- c(3, 7, 9, 10, 4, 8, 2) # 7
+#' mediana(a)
+#' mediana(b)
+#' }
+
+
+
+mediana <- function(x) {
+        x <- sort(x)
+        if (length(x) %% 2 == 0) {
+                after <- length(x)/2
+                before <- after + 1
+                
+                return((x[after] + x[before]) / 2)
+        }else{
+                idx <- ceiling(length(x) / 2)
+                return(x[idx])
+        }
+}
