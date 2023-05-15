ab6bf33d44983de12ae907a8c64bb60929ff300c
Robson Cruz <rcflorestal@yahoo.com.br>
2023-05-15 18:57:02 -0300

    Uso funcao mediana

--- Estatistica/median/main.py
+++ Estatistica/median/main.py
@@ -1,11 +1,11 @@
-def median(x):
-    """Calcula a mediana de uma lista de dados"""
-    x = sorted(x)
-    middle = int(len(x)/2)
+from mediana import mediana
 
-    if len(x) % 2 == 0:
-        m = (x[middle-1] + x[middle]) / 2
-    else:
-        m = x[middle]
 
-    return print(m)
+a = [10, 3, 4, 8, 9, 2]  # 6
+b = [3, 7, 9, 10, 4, 8, 2]  # 7
+c = [3, 5, 6, 8, 9, 12, 13, 15, 15, 16, 17]  # 12
+
+
+mediana(a)
+mediana(b)
+mediana(c)
