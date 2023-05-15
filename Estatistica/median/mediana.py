17ef9f01f451303f4d1163510963f6ebfa35b436
Robson Cruz <rcflorestal@yahoo.com.br>
2023-05-15 18:57:24 -0300

    Funcao para calcular a mediana de uma lista

---
+++ Estatistica/median/mediana.py
@@ -0,0 +1,11 @@
+def mediana(x):
+    """Calcula a mediana de uma lista de dados"""
+    x = sorted(x)
+    middle = int(len(x)/2)
+
+    if len(x) % 2 == 0:
+        m = (x[middle-1] + x[middle]) / 2
+    else:
+        m = x[middle]
+
+    return print(m)
