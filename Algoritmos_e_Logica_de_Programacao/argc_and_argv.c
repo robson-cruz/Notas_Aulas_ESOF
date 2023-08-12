a136bf2a2a82fe3e3460975c6454f63548d22e3d
Robson Cruz <rcflorestal@yahoo.com.br>
2023-08-12 10:55:53 -0300

    argc and argv

---
+++ Algoritmos_e_Logica_de_Programacao/argc_and_argv.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/***
+argc: an integer representing the number of command-line arguments passed to
+the program.
+**argv: an array of strings containing the actual arguments. 
+***/
+
+int main(int argc, char **argv) {
+	
+	int j, k;
+
+	// Iterate over the command-line arguments
+	while (argv[j]) {
+		k = 0;
+		while (argv[j][k]) {
+			printf("%c", argv[j][k]);
+			k++;
+		}
+		puts("");
+		++j;
+	}
+	
+	return 0;
+}
