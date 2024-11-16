87d83ea41d9dca10602d5acbf3c7b871a4f6c9fb
Robson Cruz <rcflorestal@yahoo.com.br>
2024-11-16 12:05:46 -0300

    Read csv file

--- Programacao_Sistemas_II/file_handling/Main.java
+++ Programacao_Sistemas_II/file_handling/Main.java
@@ -3,10 +3,13 @@ package file_handling;
 public class Main {
 
 	public static void main(String[] args) {
-		String filePath = "data/customers.txt";
+		//String filePath = "customers.txt";
+		String filePath = "pets.csv";
        
-		String data = TextFile.readerFile(filePath);
-		TextFile.head(data, 5);
+		//String data = TextFile.readerFile(filePath);
+		//TextFile.head(data, 5);
+		String data = CsvFile.readerFile(filePath);
+		CsvFile.head(data, 5);
 	}
 
 }
