078636b27289decd279c8096ee003d7b0bde970e
Robson Cruz <rcflorestal@yahoo.com.br>
2024-11-16 10:47:53 -0300

    Removed commented line

--- Programacao_Sistemas_II/file_handling/Main.java
+++ Programacao_Sistemas_II/file_handling/Main.java
@@ -5,7 +5,6 @@ public class Main {
 	public static void main(String[] args) {
 		String filePath = "data/customers.txt";
        
-        //System.out.println(TextFile.readerFile(filePath));
 		String data = TextFile.readerFile(filePath);
 		TextFile.head(data, 5);
 	}
