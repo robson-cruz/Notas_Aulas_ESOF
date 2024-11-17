d9496cb9ae661e36fcfc72e89dabbc05a48d06f8
Robson Cruz <rcflorestal@yahoo.com.br>
2024-11-17 16:58:00 -0300

    DeleteTutor class

---
+++ Programacao_Sistemas_II/clinica_veterinaria/src/main/java/crud/DeleteTutor.java
@@ -0,0 +1,28 @@
+package crud;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import model.Tutor;
+
+public class DeleteTutor {
+
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("clinica_veterinaria");
+		EntityManager em = emf.createEntityManager();
+		
+		Tutor tutor = em.find(Tutor.class, 1L);
+		
+		if (tutor != null) {
+			em.getTransaction().begin();
+			em.remove(tutor);
+			em.getTransaction().commit();
+		}
+		
+		em.close();
+		emf.close();
+
+
+	}
+
+}
