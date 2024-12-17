961b7c6ea5067822b4da2805daf1a6699b2441bd
Robson Cruz <rcflorestal@yahoo.com.br>
2024-12-17 13:05:38 -0300

    Printing the version of the Java Virtual Machine

---
+++ Programacao_Sistemas_II/JVM/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        String jvmVersion = System.getProperty("java.vm.version");
+        System.out.println("JVM Version: " + jvmVersion);
+    }
+}
