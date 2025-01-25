30902fc6bc2122b6989661a8f0156465f6f3c880
Robson Cruz <22680143+robson-cruz@users.noreply.github.com>
2025-01-25 12:50:05 -0300

    Models for database, tables and forms

---
+++ Programacao_Sistemas_II/Ativ_01/Python/models/db_model.py
@@ -0,0 +1,39 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+
+db = SQLAlchemy()  # Import the db as an instance of SQLAlchemy
+
+
+class User(UserMixin, db.Model):
+    """Representation of the User class
+    UserMixin allows us to use methods such as is_authenticated(), is_active(), is_anonymous(), and get_id().
+    """
+    __tablename__ = "user"
+    user_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(64), unique=True, nullable=False)
+    password = db.Column(db.String(300), nullable=False)
+
+    def get_id(self):
+        return str(self.user_id)  # Ensure it returns a string
+
+
+class Motorcycle(db.Model):
+    __tablename__ = "motorcycle"
+    motorcycle_id = db.Column(db.Integer, primary_key=True)
+    marca = db.Column(db.String(50), nullable=False)
+    modelo = db.Column(db.String(50), nullable=False)
+    combustivel = db.Column(db.String(50), nullable=False)
+    cor = db.Column(db.String(50), nullable=False)
+    ano = db.Column(db.Integer, nullable=False)
+    side_car = db.Column(db.String(4), nullable=True)
+
+
+class Car(db.Model):
+    __tablename__ = "car"
+    car_id = db.Column(db.Integer, primary_key=True)
+    marca = db.Column(db.String(50), nullable=False)
+    modelo = db.Column(db.String(50), nullable=False)
+    combustivel = db.Column(db.String(50), nullable=False)
+    cor = db.Column(db.String(50), nullable=False)
+    ano = db.Column(db.Integer, nullable=False)
---
+++ Programacao_Sistemas_II/Ativ_01/Python/models/forms.py
@@ -0,0 +1,46 @@
+from flask_wtf import FlaskForm
+from wtforms.fields.choices import SelectField
+from wtforms.fields.numeric import IntegerField
+from wtforms.fields.simple import StringField, SubmitField, BooleanField, PasswordField, EmailField
+from wtforms.validators import DataRequired, Length, Regexp, Email, InputRequired, NumberRange
+
+
+class UserForm(FlaskForm):
+    """Userform class to representing a form to register new users i the database"""
+    name = StringField("Nome", validators=[
+        DataRequired(),
+        Length(3, 100, message="Please provide a valid name"),
+        Regexp(
+            "^[a-zA-Z][a-zA-Z0-9_.]*$",
+            0,
+            "Nome de usuário deve conter somente letras, números, ponto ou underscore"
+        )
+    ])
+    email = EmailField("Email", validators=[DataRequired(), Email(), Length(1, 64)])
+    password = PasswordField("Senha", validators=[DataRequired(), Length(8, 300)])
+    button = SubmitField("Cadastar Usuário")
+
+
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[InputRequired(), Email(), Length(1, 64)])
+    password = PasswordField("Senha", validators=[InputRequired(), Length(min=8, max=72)])
+    button = SubmitField("Entrar")
+
+
+class MotorcycleForm(FlaskForm):
+    marca = StringField("Marca", validators=[DataRequired()])
+    modelo = StringField("Modelo", validators=[DataRequired()])
+    combustivel = StringField("Combustivel", validators=[DataRequired()])
+    cor = StringField("Cor", validators=[DataRequired()])
+    ano = IntegerField("Ano", validators=[DataRequired(), NumberRange(4)])
+    side_car = BooleanField("Side car")
+    button = SubmitField("Cadastrar Moto")
+
+
+class CarForm(FlaskForm):
+    marca = StringField("Marca", validators=[DataRequired()])
+    modelo = StringField("Modelo", validators=[DataRequired()])
+    combustivel = SelectField("Combustivel", validators=[DataRequired()], choices=["Gasolina", "Diesel", "Álcool", "Flex"])
+    cor = StringField("Cor", validators=[DataRequired()])
+    ano = IntegerField("Ano", validators=[DataRequired(), NumberRange(4)])
+    button = SubmitField("Cadastrar Carro")
